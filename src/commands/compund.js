ec86b86b9c64739ab7695165850da9203d93bfa2
Tomer Levy <tmrlvi@gmail.com>
2022-02-19 15:01:40 +0200

    Some syncing issues

--- src/commands/compund.js
+++ src/commands/compund.js
@@ -16,7 +16,7 @@ module.exports = {
             await interaction.reply({content: ":warning: *Wallet is locked*", ephemeral: true})
             return;
         }
-        interaction.deferReply({ephemeral: true});
+        await interaction.deferReply({ephemeral: true});
 
         let txParamsArg = {
             targets: [{address: info.publicAddress, amount: -1}],
@@ -25,10 +25,10 @@ module.exports = {
             networkFeeMax: 0,
             compoundingUTXO:true,
         }
-        wallet.submitTransaction(txParamsArg).then((tx) => {
-            interaction.editReply({content: `:dollar: Compounding successful ([here](${KATNIP_TX}${tx.txids[0]}))`})
-        }).catch((e) => {
-            interaction.editReply({content: `:warning: *Compounding failed.* \n> ${e}`})
+        wallet.submitTransaction(txParamsArg).then(async (tx) => {
+            await interaction.editReply({content: `:dollar: Compounding successful ([here](${KATNIP_TX}${tx.txids[0]}))`})
+        }).catch(async (e) => {
+            await interaction.editReply({content: `:warning: *Compounding failed.* \n> ${e}`})
         });
     },
 }
